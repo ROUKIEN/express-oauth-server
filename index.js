93c8f5bf0bafc672353c6de8d607d2d975dfc7ff
Rukien <julien.jean@froupit.org>
2018-05-18 19:04:21 +0200

    - Removed Bluebird promises (replaced by es6 promises)

--- index.js
+++ index.js
@@ -1,34 +1,31 @@
-'use strict';
+'use strict'
 
 /**
  * Module dependencies.
  */
-
-var InvalidArgumentError = require('oauth2-server/lib/errors/invalid-argument-error');
-var NodeOAuthServer = require('oauth2-server');
-var Promise = require('bluebird');
-var Request = require('oauth2-server').Request;
-var Response = require('oauth2-server').Response;
-var UnauthorizedRequestError = require('oauth2-server/lib/errors/unauthorized-request-error');
+const InvalidArgumentError = require('oauth2-server/lib/errors/invalid-argument-error');
+const NodeOAuthServer = require('oauth2-server');
+const Request = require('oauth2-server').Request;
+const Response = require('oauth2-server').Response;
+const UnauthorizedRequestError = require('oauth2-server/lib/errors/unauthorized-request-error');
 
 /**
  * Constructor.
  */
-
-function ExpressOAuthServer(options) {
-  options = options || {};
+function ExpressOAuthServer (options) {
+  options = options || {}
 
   if (!options.model) {
-    throw new InvalidArgumentError('Missing parameter: `model`');
+    throw new InvalidArgumentError('Missing parameter: `model`')
   }
 
-  this.useErrorHandler = options.useErrorHandler ? true : false;
-  delete options.useErrorHandler;
+  this.useErrorHandler = options.useErrorHandler ? true : false
+  delete options.useErrorHandler
 
-  this.continueMiddleware = options.continueMiddleware ? true : false;
-  delete options.continueMiddleware;
+  this.continueMiddleware = options.continueMiddleware ? true : false
+  delete options.continueMiddleware
 
-  this.server = new NodeOAuthServer(options);
+  this.server = new NodeOAuthServer(options)
 }
 
 /**
@@ -38,26 +35,21 @@ function ExpressOAuthServer(options) {
  *
  * (See: https://tools.ietf.org/html/rfc6749#section-7)
  */
+ExpressOAuthServer.prototype.authenticate = function (options) {
 
-ExpressOAuthServer.prototype.authenticate = function(options) {
-  var that = this;
+  return (req, res, next) => {
+    var request = new Request(req)
+    var response = new Response(res)
 
-  return function(req, res, next) {
-    var request = new Request(req);
-    var response = new Response(res);
-    return Promise.bind(that)
-      .then(function() {
-        return this.server.authenticate(request, response, options);
+    return Promise.resolve()
+      .then(() => this.server.authenticate(request, response, options))
+      .then(token => {
+        res.locals.oauth = { token: token }
+        next()
       })
-      .tap(function(token) {
-        res.locals.oauth = { token: token };
-        next();
-      })
-      .catch(function(e) {
-        return handleError.call(this, e, req, res, null, next);
-      });
-  };
-};
+      .catch(e => handleError.call(this, e, req, res, null, next))
+  }
+}
 
 /**
  * Authorization Middleware.
@@ -66,32 +58,25 @@ ExpressOAuthServer.prototype.authenticate = function(options) {
  *
  * (See: https://tools.ietf.org/html/rfc6749#section-3.1)
  */
+ExpressOAuthServer.prototype.authorize = function (options) {
 
-ExpressOAuthServer.prototype.authorize = function(options) {
-  var that = this;
+  return (req, res, next) => {
+    var request = new Request(req)
+    var response = new Response(res)
 
-  return function(req, res, next) {
-    var request = new Request(req);
-    var response = new Response(res);
-
-    return Promise.bind(that)
-      .then(function() {
-        return this.server.authorize(request, response, options);
-      })
-      .tap(function(code) {
-        res.locals.oauth = { code: code };
+    return Promise.resolve()
+      .then(() =>  this.server.authorize(request, response, options))
+      .then(code => {
+        res.locals.oauth = { code: code }
         if (this.continueMiddleware) {
-          next();
+          next()
         }
+        
+        handleResponse.call(this, req, res, response)
       })
-      .then(function() {
-        return handleResponse.call(this, req, res, response);
-      })
-      .catch(function(e) {
-        return handleError.call(this, e, req, res, response, next);
-      });
-  };
-};
+      .catch(e => handleError.call(this, e, req, res, response, next))
+  }
+}
 
 /**
  * Grant Middleware.
@@ -100,74 +85,66 @@ ExpressOAuthServer.prototype.authorize = function(options) {
  *
  * (See: https://tools.ietf.org/html/rfc6749#section-3.2)
  */
+ExpressOAuthServer.prototype.token = function (options) {
 
-ExpressOAuthServer.prototype.token = function(options) {
-  var that = this;
+  return (req, res, next) => {
+    var request = new Request(req)
+    var response = new Response(res)
 
-  return function(req, res, next) {
-    var request = new Request(req);
-    var response = new Response(res);
-
-    return Promise.bind(that)
-      .then(function() {
-        return this.server.token(request, response, options);
-      })
-      .tap(function(token) {
-        res.locals.oauth = { token: token };
+    return Promise.resolve()
+      .then(() => this.server.token(request, response, options))
+      .then(token => {
+        res.locals.oauth = { token: token }
         if (this.continueMiddleware) {
-          next();
+          next()
         }
+        
+        handleResponse.call(this, req, res, response);
       })
-      .then(function() {
-        return handleResponse.call(this, req, res, response);
-      })
-      .catch(function(e) {
-        return handleError.call(this, e, req, res, response, next);
-      });
-  };
-};
+      .catch(e => handleError.call(this, e, req, res, response, next))
+  }
+}
 
 /**
  * Handle response.
  */
-var handleResponse = function(req, res, response) {
+const handleResponse = function(req, res, response) {
 
   if (response.status === 302) {
-    var location = response.headers.location;
-    delete response.headers.location;
-    res.set(response.headers);
-    res.redirect(location);
+    var location = response.headers.location
+    delete response.headers.location
+    res.set(response.headers)
+    res.redirect(location)
   } else {
-    res.set(response.headers);
-    res.status(response.status).send(response.body);
+    res.set(response.headers)
+    res.status(response.status).send(response.body)
   }
-};
+}
 
 /**
  * Handle error.
  */
 
-var handleError = function(e, req, res, response, next) {
+const handleError = function(e, req, res, response, next) {
 
   if (this.useErrorHandler === true) {
-    next(e);
+    next(e)
   } else {
     if (response) {
-      res.set(response.headers);
+      res.set(response.headers)
     }
 
-    res.status(e.code);
+    res.status(e.code)
 
     if (e instanceof UnauthorizedRequestError) {
-      return res.send();
+      return res.send()
     }
 
-    res.send({ error: e.name, error_description: e.message });
+    res.send({ error: e.name, error_description: e.message })
   }
-};
+}
 
 /**
  * Export constructor.
  */
-
-module.exports = ExpressOAuthServer;
+module.exports = ExpressOAuthServer
